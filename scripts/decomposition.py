4eef8e2eefd155bdd7c1a93d778520714c6ee18a
rodrigomelopinto <rodrigo.melo.pinto@tecnico.ulisboa.pt>
2023-12-24 10:25:33 +0000

    both operations combined in one ontology and only 1 script to process all

--- scripts/decomposition.py
+++ scripts/decomposition.py
@@ -1,21 +1,4 @@
-import pandas as pd
-from rdflib import Graph, URIRef
 from datetime import datetime
-
-def read_owl_ontology(file_path):
-    g = Graph()
-    g.parse(file_path, format="xml")  # Assuming the ontology is in RDF/XML format
-    return g
-
-def extract_date_components(ontology_graph):
-    date_components = {}
-    for component in ["Year", "Month", "Day"]:
-        component_query = URIRef(f"http://example.org#{component}Query")
-        date_component_obj = ontology_graph.value(component_query, URIRef("http://example.org#hasComponent"), None)
-        if date_component_obj is not None:
-            date_component_query = str(date_component_obj)
-            date_components[component] = date_component_query
-    return date_components
 
 def infer_date_components(date_value, date_components):
     date_obj = datetime.strptime(date_value, "%Y-%m-%d")
@@ -24,43 +7,3 @@ def infer_date_components(date_value, date_components):
         query_result = date_obj.strftime(query)
         components[component] = query_result
     return components
-
-def process_csv(input_csv_path, output_csv_path, ontology_graph):
-    # Read the input CSV file
-    df = pd.read_csv(input_csv_path)
-
-    # Extract date components from the ontology
-    date_components = extract_date_components(ontology_graph)
-
-    # Create new columns for Day, Month, and Year
-    df["Day"] = ""
-    df["Month"] = ""
-    df["Year"] = ""
-
-    # Process each row in the data frame
-    for index, row in df.iterrows():
-        date_value = row["Date"]  # Assuming the "Date" column in the CSV contains date values
-
-        # Infer date components based on the date value
-        components = infer_date_components(date_value, date_components)
-
-        # Store the components for each row
-        df.at[index, "Day"] = components["Day"]
-        df.at[index, "Month"] = components["Month"]
-        df.at[index, "Year"] = components["Year"]
-
-    # Save the updated data frame to a new CSV file
-    df.to_csv(output_csv_path, index=False)
-
-# Example OWL ontology file path
-owl_file_path = "/home/rodrirocki/Thesis/ontologies/test.owl"
-
-# Read OWL ontology from the specified file
-ontology_graph = read_owl_ontology(owl_file_path)
-
-# Input CSV file path and output CSV file path
-input_csv_path = "/home/rodrirocki/Thesis/datasets/test_dataset.csv"
-output_csv_path = "/home/rodrirocki/Thesis/datasets/generated_dataset.csv"
-
-# Process the CSV file and create a new CSV file with date components
-process_csv(input_csv_path, output_csv_path, ontology_graph)
--- scripts/mapping.py
+++ scripts/mapping.py
@@ -1,11 +1,4 @@
-import pandas as pd
-from rdflib import Graph, URIRef
 from datetime import datetime
-
-def read_season_ontology(file_path):
-    g = Graph()
-    g.parse(file_path, format="xml")  # Assuming the ontology is in RDF/XML format
-    return g
 
 def infer_season(date_value, season_intervals):
     date_obj = datetime.strptime(date_value, "%Y-%m-%d")
@@ -21,43 +14,3 @@ def infer_season(date_value, season_intervals):
 
     return None  # Return None if no matching season is found
 
-def process_season_csv(input_csv_path, output_csv_path, ontology_graph):
-    # Read the input CSV file
-    df = pd.read_csv(input_csv_path)
-
-    # Extract season intervals from the ontology
-    season_intervals = {}
-    for season in ["Winter", "Spring", "Summer", "Autumn"]:
-        season_interval_obj = ontology_graph.value(URIRef(f"http://example.org#{season}"), URIRef("http://example.org#hasSeasonInterval"), None)
-        if season_interval_obj is not None:
-            season_intervals[season] = str(season_interval_obj)
-
-    # Create a new column for Season
-    df["Season"] = ""
-
-    # Process each row in the data frame
-    for index, row in df.iterrows():
-        date_value = row["Date"]  # Assuming the "Date" column in the CSV contains date values
-
-
-        # Infer season based on the date value
-        season = infer_season(date_value, season_intervals)
-
-        # Store the season for each row
-        df.at[index, "Season"] = season
-
-    # Save the updated data frame to a new CSV file
-    df.to_csv(output_csv_path, index=False)
-
-# Example season ontology file path
-season_ontology_file_path = "/home/rodrirocki/Thesis/ontologies/mapping.owl"
-
-# Read season ontology from the specified file
-season_ontology_graph = read_season_ontology(season_ontology_file_path)
-
-# Input CSV file path and output CSV file path
-season_input_csv_path = "/home/rodrirocki/Thesis/datasets/test_dataset.csv"
-season_output_csv_path = "/home/rodrirocki/Thesis/datasets/generated_dataset.csv"
-
-# Process the CSV file and create a new CSV file with the Season variable
-process_season_csv(season_input_csv_path, season_output_csv_path, season_ontology_graph)
